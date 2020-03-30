b9f145d5fa449475e3869dfde46bb09a14abc102
Sam Sheffres <samsheff@yandex.ru>
2020-03-30 14:34:13 -0700

    Fix a bunch of stuff

---
+++ SSH Xbox/Helpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SSH_Xbox
+{
+    static class Helpers
+    {
+        public static String GetLastLines(this String text, int lines)
+        {
+            return String.Join("\n", text.Split('\n').GetLast(lines));
+        }
+
+        public static T[] GetLast<T>(this T[] source, int n)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (n > source.Length)
+                return source;
+            if (n < 0)
+                throw new ArgumentOutOfRangeException(nameof(n), "Can't be negative");
+
+            var target = new T[n];
+            Array.Copy(source, source.Length - n, target, 0, n);
+            return target;
+        }
+    }
+}
+
+namespace System.Runtime
+{
+    class StringExtensions
+    {
+    }
+}
--- SSH Xbox/MainPage.xaml.cs
+++ SSH Xbox/MainPage.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Renci.SshNet;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +13,96 @@ namespace SSH_Xbox
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        SshClient client;
+        ShellStream shell;
+
+        string host = "";
+        string username = "";
+        string password = "";
+        bool connected = false;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.textBox.Focus(FocusState.Programmatic);
+
+            this.textBox.Text += "Host: ";
+
+            //IInputStream inputStream = this.shell.AsInputStream();
+            //inputStream.
+        }
+
+        private void ConnectToHost()
+        {
+            this.client = new SshClient("box.vsl.sh", "root", "yL5,dmAEh(fd!N)s");
+            this.textBox.Text += "\nConnecting to Server...";
+            this.client.Connect();
+            this.textBox.Text += "\nConnected! Opening Shell Stream...";
+            this.shell = this.client.CreateShellStream("xbox", 256, 512, (uint)((Frame)Window.Current.Content).ActualWidth, (uint)((Frame)Window.Current.Content).ActualHeight, 512);
+
+            this.connected = true;
+
+            readLoop();
+        }
+
+        private async void readLoop()
+        {
+            while (true)
+            {
+                if (this.shell.Length > 0)
+                {
+                    var result = new byte[this.shell.Length];
+                    await this.shell.ReadAsync(result, 0, (int)this.shell.Length);
+
+                    string console = this.textBox.Text += System.Text.Encoding.ASCII.GetString(result);
+
+                    this.textBox.Text = console.GetLastLines(GetNumberOfVerticalLines());
+                } else
+                {
+                    await Task.Delay(100);
+                }
+            }
+        }
+
+        private int GetNumberOfVerticalLines()
+        {
+            return (int)((((Frame)Window.Current.Content).ActualHeight - textBox.ActualHeight) / 5);
+        }
+
+        private void textBox1_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+
+                if (connected == false)
+                {
+                    if (host == "")
+                    {
+                        host = textBox1.Text;
+                        textBox1.Text = "";
+                        textBox.Text += "\nUsername: ";
+                    } else if (username == "") {
+                        username = textBox1.Text;
+                        textBox1.Text = "";
+                        textBox.Text += "\nPassword: ";
+                    } else if (password == "")
+                    {
+                        password = textBox1.Text;
+                        textBox1.Text = "";
+                        ConnectToHost();
+                    }
+                } else
+                {
+                    string cmdToExec = textBox1.Text;
+                    textBox.Text += "\n";
+                    textBox1.Text = "";
+
+                    this.shell.WriteLine(cmdToExec);
+                }
+
+            }
         }
     }
 }
